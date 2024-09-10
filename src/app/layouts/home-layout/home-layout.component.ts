cb4c7f8771c8a77b1d8c35fa8afe5c065f47148e
Eduard David <eduard.david0129@gmail.com>
2024-09-10 12:41:23 +0300

    (feat): Home page layout

---
+++ src/app/layouts/home-layout/home-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-layout',
+  template: `
+    <p>
+      home-layout works!
+    </p>
+  `,
+  styleUrls: ['./home-layout.component.scss']
+})
+export class HomeLayoutComponent {
+
+}
