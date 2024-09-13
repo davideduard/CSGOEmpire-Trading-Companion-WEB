95324fe64fdba87299e328574840e4f9a74ebd1b
Eduard David <eduard.david0129@gmail.com>
2024-09-13 16:57:44 +0300

    (feat): Implemented error handling on registration form

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,7 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RegisterRequest } from '../../types';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { F } from '@angular/cdk/keycodes';
+import {
+	matchFieldValidator,
+	passwordValidator,
+	usernameValidator
+} from '../../../../validators';
 
 @Component({
 	selector: 'app-register',
@@ -17,6 +21,7 @@ import { F } from '@angular/cdk/keycodes';
 						<app-text-field
 							label="username"
 							[formControl]="usernameControl"
+							[errorMessage]="getUsernameErrorMessage()"
 						></app-text-field>
 
 						<app-text-field
@@ -28,18 +33,21 @@ import { F } from '@angular/cdk/keycodes';
 						<app-text-field
 							label="confirm email"
 							[formControl]="confirmEmailControl"
+							[errorMessage]="getConfirmEmailErrorMessage()"
 						></app-text-field>
 
 						<app-text-field
 							label="password"
 							type="password"
 							[formControl]="passwordControl"
+							[errorMessage]="getPasswordErrorMessage()"
 						></app-text-field>
 
 						<app-text-field
 							label="confirm password"
 							type="password"
 							[formControl]="confirmPasswordControl"
+							[errorMessage]="getConfirmPasswordErrorMessage()"
 						></app-text-field>
 
 						<div class="w-56 mt-5">
@@ -69,15 +77,24 @@ import { F } from '@angular/cdk/keycodes';
 	styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-	//Register form control
-	usernameControl = new FormControl('', [Validators.required]);
+	usernameControl = new FormControl('', [
+		Validators.required,
+		usernameValidator()
+	]);
 	emailControl = new FormControl('', [Validators.required, Validators.email]);
 	confirmEmailControl = new FormControl('', [
 		Validators.required,
-		Validators.email
+		Validators.email,
+		matchFieldValidator('email')
 	]);
-	passwordControl = new FormControl('', [Validators.required]);
-	confirmPasswordControl = new FormControl('', [Validators.required]);
+	passwordControl = new FormControl('', [
+		Validators.required,
+		passwordValidator()
+	]);
+	confirmPasswordControl = new FormControl('', [
+		Validators.required,
+		matchFieldValidator('password')
+	]);
 
 	registerForm = new FormGroup({
 		username: this.usernameControl,
@@ -93,23 +110,82 @@ export class RegisterComponent {
 		new EventEmitter<RegisterRequest>();
 
 	onRegister() {
-		this.registerRequested.emit({
-			email: this.emailControl.value!,
-			username: this.usernameControl.value!,
-			password: this.passwordControl.value!
-		});
+		this.registerForm.markAllAsTouched();
+
+		if (this.registerForm.valid) {
+			this.registerRequested.emit({
+				email: this.emailControl.value!,
+				username: this.usernameControl.value!,
+				password: this.passwordControl.value!
+			});
+		}
 	}
 
 	onLogin(): void {
 		this.loginRequested.emit();
 	}
 
-	getEmailErrorMessage() {
-		if (this.emailControl.hasError('required')) {
-			return 'Email is required';
-		} else if (this.emailControl.hasError('email')) {
-			return 'Please enter a valid email';
+	getUsernameErrorMessage(): string {
+		if (this.usernameControl.hasError('required')) {
+			return 'Username field cannot be empty';
 		}
+
+		if (this.usernameControl.hasError('invalidUsername')) {
+			return 'Username should be at least 3 characters long and can only contain letters, numbers and allowed special characters (-, _, .)';
+		}
+
+		return '';
+	}
+
+	getEmailErrorMessage(): string {
+		if (this.emailControl.hasError('required')) {
+			return 'Email field cannot be empty';
+		}
+
+		if (this.emailControl.hasError('email')) {
+			return 'Please use a valid email format';
+		}
+
+		return '';
+	}
+
+	getConfirmEmailErrorMessage(): string {
+		if (this.confirmEmailControl.hasError('required')) {
+			return 'Please confirm your email';
+		}
+
+		if (this.confirmEmailControl.hasError('email')) {
+			return 'Please use a valid email format';
+		}
+
+		if (this.confirmEmailControl.hasError('mismatch')) {
+			return 'Emails do not match';
+		}
+
+		return '';
+	}
+
+	getPasswordErrorMessage(): string {
+		if (this.passwordControl.hasError('required')) {
+			return 'Password field cannot be empty';
+		}
+
+		if (this.passwordControl.hasError('weakPassword')) {
+			return 'The password must be at least 8 characters long, contain at least 1 capital letter, 1 number and 1 special character (!, @, #, $, %, ^, &, *, ., ?)';
+		}
+
+		return '';
+	}
+
+	getConfirmPasswordErrorMessage(): string {
+		if (this.confirmPasswordControl.hasError('required')) {
+			return 'Please confirm your password';
+		}
+
+		if (this.confirmPasswordControl.hasError('mismatch')) {
+			return 'Passwords do not match';
+		}
+
 		return '';
 	}
 }
